3e25444712e6863afdf2b047323ce188830f79cc
Muhaiminul Islam <156056627+Muhaiminul-official@users.noreply.github.com>
2025-04-03 12:07:14 +0600

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,8 +8,9 @@ import { useClerk, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
 
-  const { isSeller, router,user } = useAppContext();
+  const { router,user } = useAppContext();
   const { openSignIn } = useClerk();
+  const {isSeller} = true;
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
